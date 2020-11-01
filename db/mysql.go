902dae71fa4cac9203f4f4769dc7f62ea6fced33
Dan Burke <dburke@addictmud.org>
2020-11-01 11:51:01 -0500

    fix mysql dsn

--- db/mysql.go
+++ db/mysql.go
@@ -11,13 +11,13 @@ import (
 var conn *gorm.DB
 
 func init() {
-	viper.SetDefault("db.conn.maxidleconnections", 5)
-	viper.SetDefault("db.conn.maxopenconnections", 20)
-	viper.SetDefault("db.conn.host", "127.0.0.1")
-	viper.SetDefault("db.conn.port", 3306)
-	viper.SetDefault("db.conn.user", "")
-	viper.SetDefault("db.conn.name", "")
-	viper.SetDefault("db.conn.pass", "")
+	viper.SetDefault("db.mysql.maxidleconnections", 5)
+	viper.SetDefault("db.mysql.maxopenconnections", 20)
+	viper.SetDefault("db.mysql.host", "127.0.0.1")
+	viper.SetDefault("db.mysql.port", 3306)
+	viper.SetDefault("db.mysql.user", "")
+	viper.SetDefault("db.mysql.name", "")
+	viper.SetDefault("db.mysql.pass", "")
 }
 
 func Open() *gorm.DB {
@@ -28,12 +28,12 @@ func Open() *gorm.DB {
 
 	var err error
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%port)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		viper.GetString("db.conn.user"),
-		viper.GetString("db.conn.pass"),
-		viper.GetString("db.conn.host"),
-		viper.GetInt("db.conn.port"),
-		viper.GetString("db.conn.name"),
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		viper.GetString("db.mysql.user"),
+		viper.GetString("db.mysql.pass"),
+		viper.GetString("db.mysql.host"),
+		viper.GetInt("db.mysql.port"),
+		viper.GetString("db.mysql.name"),
 	)
 
 	conn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -46,8 +46,8 @@ func Open() *gorm.DB {
 	if dbh, err := conn.DB(); err != nil {
 		panic(err)
 	} else {
-		dbh.SetMaxIdleConns(viper.GetInt("db.conn.maxidleconnections"))
-		dbh.SetMaxOpenConns(viper.GetInt("db.conn.maxopenconnections"))
+		dbh.SetMaxIdleConns(viper.GetInt("db.mysql.maxidleconnections"))
+		dbh.SetMaxOpenConns(viper.GetInt("db.mysql.maxopenconnections"))
 	}
 
 	return conn
